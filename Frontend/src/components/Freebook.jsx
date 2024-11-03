3170c6dfb3ea88e5f13852bba29ec7629b68b634
Sanjay Rawat <122571313+ssrlife424@users.noreply.github.com>
2024-11-04 00:23:24 +0530

    Update Freebook.jsx

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -9,7 +9,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://books-library-backend.onrender.com/book");
         const data = res.data.filter((data) => data.category === "Free")
         console.log(data)
         setBook(data);
