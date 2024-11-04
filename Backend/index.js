4ce0368fea4fd68008a862512ca49e3dcfd90dd7
Sanjay Rawat <122571313+ssrlife424@users.noreply.github.com>
2024-11-05 01:35:49 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(process.cwd(), "client/build")));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://books-library-alpha.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
