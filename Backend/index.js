6833997761cabd252fdd10bb66b46a03d3c3941d
Sanjay Rawat <Sanjayrawat892@gmail.com>
2024-11-04 14:41:48 +0530

    few changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,27 @@
-
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import bookRoute from "./route/book.route.js"
-import userRoute from './route/user.route.js'
-app.use(express.json())
-
-dotenv.config();
+import bookRoute from "./route/book.route.js";
+import userRoute from "./route/user.route.js";
 const app = express();
 app.use(express.json());
-app.use(cors());
 
+dotenv.config();
+app.use(
+  cors({
+    origin:
+      "https://books-library-cq7yel0t1-sanjay-rawats-projects.vercel.app/", // Replace with your front-end domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 const PORT = process.env.PORT || 4000;
-console.log(`Using PORT: ${PORT}`);
 const URI = process.env.MongoDBURI;
 
-
 // connection to mongodb
-mongoose.connect(URI)
+mongoose
+  .connect(URI)
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -28,9 +31,8 @@ mongoose.connect(URI)
 // Defining Routes
 
 app.use("/book", bookRoute);
-app.use('/user', userRoute)
+app.use("/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`server is  listening on port ${PORT}`);
 });
-
--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 function  Cards({item}) {
-    console.log(item)
   return (
     <div>
       
